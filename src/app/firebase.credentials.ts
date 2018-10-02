335721d8e3a79d5d0fbf64333c5eb91a1e6325b3
Bitar youssef <bitardev@outlook.com>
2018-10-02 21:30:06 +0100

    Remove credentials file from the project

--- src/app/firebase.credentials.ts
+++ src/app/firebase.credentials.ts
@@ -1,8 +1,8 @@
 export const FIREBASE_CONFIG = {
-    apiKey: "AIzaSyB5JT2_vkXAhWVpHcshRghsf-GMItZSZ94",
-    authDomain: "shoppinglist-2d128.firebaseapp.com",
-    databaseURL: "https://shoppinglist-2d128.firebaseio.com",
-    projectId: "shoppinglist-2d128",
-    storageBucket: "shoppinglist-2d128.appspot.com",
-    messagingSenderId: "583560444299"
+    apiKey: "your-apiKey",
+    authDomain: "your-authDomain",
+    databaseURL: "your-databaseURL",
+    projectId: "your-projectId",
+    storageBucket: "your-storageBucket",
+    messagingSenderId: "your-messagingSenderId"
   };
